22136ec19efb67845a22f881a6d47546a451e922
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-14 20:26:38 +0200

    RTL8189ES

---
+++ compile_modules.sh
@@ -0,0 +1,1 @@
+make modules
--- upload.sh
+++ upload.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-HOST=192.168.0.99
+HOST=192.168.0.43
 cp arch/mips/boot/uImage.lzma kernel.bin
+cp drivers/net/wireless/rtl818x/rtl8188eu/wlan.ko rtl8189es.bin
 ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard/ kernel.bin
+ftp-upload -h ${HOST} -u root --password ismart12 -d /driver rtl8189es.bin
