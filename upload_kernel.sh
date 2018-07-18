c40ffa02bae30a9ff37092798674aaa383344e48
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-18 23:08:13 +0200

    Added Audio Support

--- upload_kernel.sh
+++ upload_kernel.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-HOST=192.168.0.46
+HOST=192.168.0.99
 cp arch/mips/boot/uImage.lzma kernel.bin
-ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard/ kernel.bin
+ftp-upload -h ${HOST} -u root --password ismart12 -d / kernel.bin
