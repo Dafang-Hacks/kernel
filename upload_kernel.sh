4aed2ba5e8119f7a97a445d8cb27e8f53b36649b
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-22 11:50:03 +0200

    RTL Drivers

--- upload_kernel.sh
+++ upload_kernel.sh
@@ -2,3 +2,8 @@
 HOST=192.168.0.99
 cp arch/mips/boot/uImage.lzma kernel.bin
 ftp-upload -h ${HOST} -u root --password ismart12 -d / kernel.bin
+
+
+
+## Load with
+# ext4load mmc 0:1 0x80600000 kernel-new.bin;bootm 0x80600000
