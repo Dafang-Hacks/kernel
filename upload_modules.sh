92474a3f3ac6935eccc76d2f8d68ae3c5538e4e2
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-22 10:44:32 +0200

    Upload Modules

--- upload_modules.sh
+++ upload_modules.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-HOST=192.168.0.46
+HOST=192.168.0.99
 cp drivers/net/wireless/rtl818x/rtl8188eu/rtl8189es.ko rtl8189es.ko
 cp sound/sound_firmware.ko audio.ko
 
-ftp-upload -h ${HOST} -u root --password ismart12 -d /lib/modules/3.10.14/ rtl8189es.ko
-ftp-upload -h ${HOST} -u root --password ismart12 -d /lib/modules/3.10.14/ audio.ko
+ftp-upload -h ${HOST} -u root --password ismart12 -d /lib/modules/3.10.108/ rtl8189es.ko
