a2b7ab6fe9d2e1511088ddae55c35aa6b3f3646d
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-08-05 00:51:48 +0200

    GPIOTest

--- ingenic_drivers/misc/gpiotest/gpiotest.c
+++ ingenic_drivers/misc/gpiotest/gpiotest.c
@@ -2,7 +2,8 @@
     #include <linux/module.h>  
     #include <linux/kernel.h>  
     #include <linux/gpio.h>                 // Required for the GPIO functions  
-    #include <linux/interrupt.h>            // Required for the IRQ code  
+    #include <linux/interrupt.h>            // Required for the IRQ code
+    #include <linux/delay.h>
       
     MODULE_LICENSE("GPL");  
     MODULE_AUTHOR("Test");  
@@ -13,17 +14,35 @@
        int result = 0;  
 
 
-       int i;
+       int i,j,pin;
        int err;
        printk(KERN_INFO "GPIO_TEST: Initializing the GPIO_TEST\n");
-       for(i = 0; i < 120;i++){
-            err = gpio_request(i, "sysfs");
-            if(err==0){
-                gpio_free(i);
-            }else{
-                printk(KERN_INFO "GPIO_TEST: Pin %d is busy\n", i);
-            }
+
+       for(i = 0; i < 4;i++){
+           for(j = 3; j < 32;j++){
+                pin = 32 * i + j;
+                printk(KERN_INFO "Requesting Pin %d \n", pin);
+                msleep(1000);
+                if(
+                pin == 13
+
+                ){
+                    printk(KERN_INFO "GPIO_TEST: Pin %d override\n", pin);
+                    continue;
+                }
+                if(gpio_is_valid(pin)==true){
+                    err = gpio_request(pin, "test");
+                    if(err==0){
+                        gpio_free(pin);
+                    }else{
+                        printk(KERN_INFO "GPIO_TEST: Pin %d is busy\n", pin);
+                    }
+
+                }
+
+           }
        }
+
        return result;  
     }  
       
---
+++ ingenic_drivers/misc/gpiotest/makeandupload.sh
@@ -0,0 +1,2 @@
+make
+ftp-upload -h 192.168.0.99 -u root --password ismart12 -d /lib/modules/3.10.14/ gpio_test.ko
